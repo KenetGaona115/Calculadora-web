da681e8cd0a8950dded2b20bcf7e2d2f215f5aef
KenetGaona115 <aldrinkenet@hotmail.com>
2022-01-19 18:15:09 -0600

    init

---
+++ Calculadora/src/app/app.component.ts
@@ -0,0 +1,101 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'Calculadora';
+  numeros = '';
+
+  operadores = ['+', '-', '*', '/'];
+
+  addNumer(numero: string) {
+    if (this.numeros == '') {
+      if (numero != '0')
+        this.numeros = this.numeros + numero;
+    } else {
+      this.numeros = this.numeros + numero;
+    }
+  }
+
+  addPoint() {
+    if (this.numeros != '' && !this.validatePointer()) {
+      this.numeros = this.numeros + '.';
+    }
+  }
+
+  addOperator(operador: string) {
+    if (this.numeros != '' && this.validateLastIndex()) {
+      this.numeros = this.numeros + operador;
+    }
+  }
+
+  result() {
+
+  }
+
+  reset() {
+    this.numeros = '';
+  }
+
+  private validateLastIndex() {
+    const char = this.numeros.charAt(this.numeros.length - 1);
+    console.log(char)
+    return !this.operadores.includes(char)
+  }
+
+  private validatePointer() {
+    const char = this.numeros.charAt(this.numeros.length - 1);
+    return this.validateLastIndex() && char == '.'
+
+  }
+
+}
+/*
+  numeros:string[] = [];
+
+  operadores = ['+', '-', '*', '/'];
+
+  addNumer(numero: string) {
+    if (this.numeros.length == 0) {
+      if (numero != '0')
+        this.numeros.push(numero) ;
+    } else {
+      this.numeros.push(numero) ;
+    }
+  }
+
+  addPoint() {
+    if (this.numeros.length != 0 && !this.validatePointer()) {
+      this.numeros.push('.')
+    }
+  }
+
+  addOperator(operador: string) {
+    if (this.numeros.length != 0 && this.validateLastIndex()) {
+      this.numeros.push(operador) ;
+    }
+  }
+
+  result() {
+
+  }
+
+  reset() {
+    this.numeros = [];
+  }
+
+  private validateLastIndex() {
+    const char = this.numeros[this.numeros.length-1];
+    console.log(char)
+    return !this.operadores.includes(char) && char == '.'
+  }
+
+  private validatePointer() {
+      const char = this.numeros[this.numeros.length-1];
+      return this.validateLastIndex() && char == '.'
+      
+  }
+*/
